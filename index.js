f469c3b39acd16fcc0f242eb3e3ea4add624948d
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-11 16:24:56 -0600

    Updated index.js
    added test array, two endpoints

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const express = require("express");
+morgan = require("morgan");  //logging requests
 const app = express();
+app.use(morgan("common"));
+
+let paraMovies = [
+    {
+        title: "The Exorcist",
+        director: "William Friedkin"
+    },
+    {
+        title: "The Conjuring",
+        director: "James Wan"
+    },
+    {
+        title: "Poltergeist",
+        director: "Tobe Hooper"
+    },
+];
+
+/* HTTP requests */
+
+app.get("/", (req, res) => {
+    res.send("Welcome to ParaFlix!")
+});
+
+app.get("/movies", (req, res) => {
+    res.json(paraMovies);
+});
+
+/* HTTP requests */
+
+app.listen(8080, () => {
+    console.log("Your app is listening on port 8080.");
+});
