013d9d06bcc1bb243f9f344c35a2a5d3db351462
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-12 14:37:51 -0600

    Created err handling function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 morgan = require("morgan");  //logging requests
 const app = express();
 app.use(morgan("common"));
+app.use(express.static('public')); //serves static file
+
 
 let paraMovies = [
     {
@@ -18,18 +20,30 @@ let paraMovies = [
     },
 ];
 
-/* HTTP requests */
-
+/* Routes user to homepage */
 app.get("/", (req, res) => {
     res.send("Welcome to ParaFlix!")
 });
 
+/* Routes developer to instructions */
+app.get("/documentation", (req, res) => {
+    res.sendFile("public/documentation.html", { 
+        root: __dirname
+    });
+});
+
+/* Routes user to movie list array */
 app.get("/movies", (req, res) => {
     res.json(paraMovies);
 });
 
-/* HTTP requests */
+/* Error handling function */
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send("Something is not right!");
+});
 
+/* Listening for requests */
 app.listen(8080, () => {
     console.log("Your app is listening on port 8080.");
 });
