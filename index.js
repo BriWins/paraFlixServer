954adb3810282b18fac5871b280b850885277627
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-20 15:24:53 -0600

    Installed CORS

--- index.js
+++ index.js
@@ -105,9 +105,9 @@ app.delete("/users/:Username", passport.authenticate("jwt", { session: false }),
     Users.findOneAndRemove({ Username: req.params.Username })
         .then((user) => {
             if (!user) {
-                res.status(400).send(req.params.Username + " was not found");
+                res.status(400).send("Username " + req.params.Username + " was not found");
             } else {
-                res.status(200).send(req.params.Username + " was deleted");
+                res.status(200).send("Username " + req.params.Username + " was deleted");
             }
         }).catch((err) => {
             console.error(err);
