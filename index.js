68b87df0d9d7b2578c1e0150a0fd8db82659bbb5
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-26 12:53:24 -0600

    Removed auth from /movies endpoint for testing

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ app.get("/documentation", (req, res) => {
 });
 
 /* Routes user to movie list array */
-app.get("/movies", passport.authenticate("jwt", { session: false }), (req, res) => {
+app.get("/movies", (req, res) => {
    Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
